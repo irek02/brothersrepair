ae7e7485722a0eeed2a11203b657e999b4737941
Irek Mirgaleev <irek.mirgaleev@acquia.com>
2017-04-22 11:50:49 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 git clone https://github.com/irek02/brothersrepair.git /tmp/brothersrepair
 
+rm -rf /var/www/html/*
+
 cp -r /tmp/brothersrepair/html/* /var/www/html
 cp -r /tmp/brothersrepair/assets /var/www/html
 
